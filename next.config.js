07e1ffc34a3fceccba3c39dfb859987fee920c6f
Dana7afez <dana7afez@gmail.com>
2025-05-15 20:20:28 +0300

    Fix dynamic route for static export

--- next.config.js
+++ next.config.js
@@ -3,16 +3,6 @@ const nextConfig = {
   output: "export",
   basePath: "/atrs-website",
   assetPrefix: "/atrs-website/",
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
-  trailingSlash: true,
 }
 
 module.exports = nextConfig
